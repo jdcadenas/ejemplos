a0c0b5432a0bbf3a490e872673a7af3b2e9e5c45
jdcadenas <jdcadenas@gmail.com>
2026-05-02 08:47:33 -0400

    ejemplo1 de variables e input

---
+++ sesion1/ejemplo01.py
@@ -0,0 +1,34 @@
+# Ejemplo de Python para Formaci'on complementaria
+x = 5
+y = 10
+
+print ("El valor de 5 * 10 es: ", x * y)
+print("realizado")
+
+nombre = input("coloca tu primer nombre ")
+apellido = input("coloca apellido ")
+
+print ("El nombre completo es: " + nombre + " " + apellido )
+
+edad = int(input("ingrese su edad "))
+
+total = edad + 1500
+print( total )
+
+if ( edad < 18 ):
+    print(" Puede entrar al cine Adulto  ")
+else:
+    print ("---- !!!!! OJO !!!!!! ---- ")
+
+""" Otra cosa """
+
+"""Funciones simples con parámetros y retorno."""
+
+def saludar(nombre):
+    """Retorna un saludo simple."""
+    return f"Hola, {nombre}"
+
+
+
+
+
